5367b952de0433238c3ee477a084c1c913c3c793
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-10-29 19:05:44 +0600

    bridge design patterns

---
+++ src/main/java/com/java/designpatterns/bridge/example/remotes/AdvancedRemote.java
@@ -0,0 +1,15 @@
+package com.java.designpatterns.bridge.example.remotes;
+
+import com.java.designpatterns.bridge.example.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
